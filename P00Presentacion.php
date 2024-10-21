3cc4d21390f333f2dc12cb00154a578b0191bb90
DSantiMGarrido03 <diego.martinez.garrido@cbtis89.edu.mx>
2024-10-21 09:31:04 -0600

    Add files via upload

---
+++ P00Presentacion.php
@@ -0,0 +1,19 @@
+<?php
+/*CBTIS 89
+P00Presentación.php
+Datos personales
+Diego Santiago Martínez Garrido
+3°A Programaíón Matutino
+*/
+
+
+echo "Datos Personales de : Diego Santiago Martínez Garrido";
+echo "<p>";
+echo "Escuela: CBTIS 89";
+echo "<p>";
+echo "Especialidad: Programación";
+echo "<p>";
+echo "Semestre y Grupo: 3°A Matutino";
+echo "<p>";
+echo "Vivo en la ciudad de Durango,Dgo";
+?>
